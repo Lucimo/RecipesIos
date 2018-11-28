0dc0cc1f00069156d66c84adee6b6658cb53172e
LUCAS PAJARES PRIETO <lucas.pajares@utmac0007.local>
2018-11-28 11:32:50 +0100

    Initial Commit

---
+++ RecetaçaosTests/Recetac_aosTests.swift
@@ -0,0 +1,36 @@
+//
+//  Recetac_aosTests.swift
+//  RecetaçaosTests
+//
+//  Created by LUCAS PAJARES PRIETO on 28/11/18.
+//  Copyright © 2018 LUCAS PAJARES PRIETO. All rights reserved.
+//
+
+import XCTest
+@testable import Recetac_aos
+
+class Recetac_aosTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
